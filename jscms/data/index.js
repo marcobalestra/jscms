3a872085bd999939397ccab479313d51b7c9c016
Marco Balestra <balestra@elatersoftware.it>
2021-05-04 16:45:46 +0200

    Edit menu

--- jscms/data/index.js
+++ jscms/data/index.js
@@ -1,7 +1,7 @@
 {
 	"title" : "Hello",
 	"blocks" : [
-		{"text":"Works in progress.","type":"text", "wrap":"<p style=\"font-weight:bold;font-style:italic;\"></p>" },
+		{"text":"Work in progress.","type":"text", "wrap":"<p style=\"font-weight:bold;font-style:italic;\"></p>" },
 		{"html":"<p>This is the home page of a new Javascript-based CMS currently under development.</p><p>It will require only a minimal server-side effort, while data storage will rely on JSON files.</p>","type":"html"}
 	]
 }
