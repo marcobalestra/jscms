231339da55e5dd82fb206a1e5b407dacc4a35916
Marco Balestra <balestra@elatersoftware.it>
2021-05-04 20:04:10 +0200

    Confirm resume editing

--- jscms/data/index.js
+++ jscms/data/index.js
@@ -1,5 +1,5 @@
 {
-	"title" : "",
+	"title" : "Hello",
 	"blocks" : [
 		{"text":"Work in progress.","type":"text", "wrap":"<h4></h4>" },
 		{"html":"<p>This is the home page of a new opensource Javascript-based CMS, currently under development.</p><p>It will require only a minimal server-side effort, while data storage will rely on JSON files.</p>","type":"html"}
