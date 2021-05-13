3b038d227e3b60081d26beca7e9f614fc1f88ed2
Marco Balestra <balestra@elatersoftware.it>
2021-05-13 15:12:50 +0200

    Subpages

--- jscms/libs/as-form-jcplugins.js
+++ jscms/libs/as-form-jcplugins.js
@@ -51,6 +51,7 @@ AS.form.fields.jcpage = class extends AS.form.field {
 		let av = v[this.prop.name];
 		let avs = av.page + ( av.id ? av.id : '');
 		this.setValue(av);
+		console.log( $( this.fakeField() ), avs );
 		window.setTimeout( ()=>{$( this.fakeField() ).val(avs).trigger('change');},100);
 		this.hideWarning();
 	};
