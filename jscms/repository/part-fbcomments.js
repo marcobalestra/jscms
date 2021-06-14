b28455a753ba7005de0dc8eac36e828635bf5c2d
Marco Balestra <balestra@elatersoftware.it>
2021-06-14 08:58:57 +0200

    FB plugin, multiple admins

--- jscms/repository/part-fbcomments.js
+++ jscms/repository/part-fbcomments.js
@@ -48,13 +48,13 @@
 			if ( ! nocomment ) {
 				$out.append(`<div class="fb-comments" data-href="${location.href.replace(/^[^:]+:../,'')}" data-width="550" data-numposts="${data.numposts}"></div>`);
 				$('meta[property="fb:admins"],meta[property="fb:app_id"]',head).remove();
-				let ids = data.fbid.replace(/[^0-9]+/g,' ').split(' ');
-				if ( data.mod == 'A') {
-					$(head).append(`<meta property="fb:app_id" content="${ ids[0] }"/>`);
-				} else {
-					ids.forEach( i => {
-						$(head).append(`<meta property="fb:admins" content="${ i }"/>`);
-					} );
+				if ( data.fbid ) {
+					let ids = data.fbid.replace(/[^0-9]+/g,' ').split(' ');
+					if ( data.mod == 'A') {
+						$(head).append(`<meta property="fb:app_id" content="${ ids[0] }"/>`);
+					} else {
+						ids.forEach( i => { $(head).append(`<meta property="fb:admins" content="${ i }"/>`); } );
+					}
 				}
 			}
 			if ( ! $(document.body).data('jcFbcommentsEvent') ) {
